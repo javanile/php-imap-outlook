2038b2c577298b2ce4437f021319d419e8184c2a
francescobianco <info.francescobianco@gmail.com>
2022-08-28 11:44:34 +0200

    Release

--- public/index.php
+++ public/index.php
@@ -94,5 +94,7 @@ if (isset($_SESSION['auth']['access_token'])) {
 
 <pre>$_SESSION['user'] = <?php var_dump($_SESSION['user']); ?></pre>
 
+
+<a href="https://github.com/javanile/php-imap-outlook" target="_blank"><img src="https://github.blog/wp-content/uploads/2008/12/forkme_right_green_007200.png?resize=149%2C149" style="position:absolute;top:0;right:0;border:0;" width="149" height="149" class="attachment-full size-full" alt="Fork me on GitHub" data-recalc-dims="1" /></a>
 </body>
 </html>
